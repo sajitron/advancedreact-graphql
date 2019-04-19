eec87df0387204be82529beeaf43a71924710c00
sajicode <sajioloye@gmail.com>
2019-04-19 22:51:46 +0100

    setup server side update mutations

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -5,6 +5,7 @@ import ItemStyles from "./styles/ItemStyles";
 import Link from "next/link";
 import PriceTag from "./styles/PriceTag";
 import formatMoney from "../lib/formatMoney";
+import DeleteItem from "./DeleteItem";
 
 export default class Item extends Component {
   static propTypes = {
@@ -38,7 +39,7 @@ export default class Item extends Component {
             <a>Edit</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete</button>
+          <DeleteItem>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
     );
