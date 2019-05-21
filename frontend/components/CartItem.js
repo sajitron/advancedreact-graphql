6e140a40f7b17c5d7f8de96176b79e820c8351df
sajicode <sajioloye@gmail.com>
2019-05-21 23:37:42 +0100

    sort deleted items issue

--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -19,22 +19,32 @@ const CartItemStyles = styled.li`
   }
 `;
 
-const CartItem = ({ cartItem }) => (
-  <CartItemStyles>
-    <img width="100" src={cartItem.item.image} alt={cartItem.item.title} />
-    <div className="cart-item-details">
-      <h3>{cartItem.item.title}</h3>
-      <p>
-        <em>
-          {cartItem.quantity} &times; {formatMoney(cartItem.item.price)}
-        </em>
-        {" - "}
-        {formatMoney(cartItem.item.price * cartItem.quantity)}
-      </p>
-    </div>
-    <RemoveFromCart id={cartItem.id} />
-  </CartItemStyles>
-);
+const CartItem = ({ cartItem }) => {
+  // first check if that item exists
+  if (!cartItem.item)
+    return (
+      <CartItemStyles>
+        This item has been removed
+        <RemoveFromCart id={cartItem.id} />
+      </CartItemStyles>
+    );
+  return (
+    <CartItemStyles>
+      <img width="100" src={cartItem.item.image} alt={cartItem.item.title} />
+      <div className="cart-item-details">
+        <h3>{cartItem.item.title}</h3>
+        <p>
+          <em>
+            {cartItem.quantity} &times; {formatMoney(cartItem.item.price)}
+          </em>
+          {" - "}
+          {formatMoney(cartItem.item.price * cartItem.quantity)}
+        </p>
+      </div>
+      <RemoveFromCart id={cartItem.id} />
+    </CartItemStyles>
+  );
+};
 
 CartItem.propTypes = {
   cartItem: PropTypes.object.isRequired
