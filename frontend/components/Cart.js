6a55c55e5de301a747f9a2d3fcaa2287dcf12415
sajicode <sajioloye@gmail.com>
2019-05-21 23:52:30 +0100

    clean up render prop

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Query, Mutation } from "react-apollo";
+import {} from "react-adopt"; // used to deal with render props issues
 import gql from "graphql-tag";
 import User from "./User";
 import CartStyles from "./styles/CartStyles";
@@ -22,49 +23,53 @@ const TOGGLE_CART_MUTATION = gql`
   }
 `;
 
+/* eslint-disable */
+const Composed = adopt({
+  // we could easily return just <User />, <Mutation />, <Query /> as the value, but we want to circumvent the proptype error
+  user: ({ render }) => <User>{render}</User>,
+  toggleCart: ({ render }) => (
+    <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>
+  ),
+  localState: ({ render }) => <Query query={LOCAL_STATE_QUERY}>{render}</Query>
+});
+/* eslint-enable */
+
 // * Since we passed the result of the current user query to its potential children, we can access them here
 
 const Cart = () => (
-  <User>
-    {({ data: { me } }) => {
+  <Composed>
+    {({ user, toggleCart, localState }) => {
+      const me = user.data.me;
       if (!me) return null;
       return (
-        <Mutation mutation={TOGGLE_CART_MUTATION}>
-          {toggleCart => (
-            <Query query={LOCAL_STATE_QUERY}>
-              {({ data, loading }) => (
-                <>
-                  {loading ? (
-                    <p>Loading data</p>
-                  ) : (
-                    <CartStyles open={data.cartOpen}>
-                      {/* we are able to toggle the cart on the apollo state from here */}
-                      <header>
-                        <CloseButton title="close" onClick={toggleCart}>
-                          &times;
-                        </CloseButton>
-                        <Supreme>{me.name}'s Cart</Supreme>
-                        <p>You have {me.cart.length} Item(s) in your cart.</p>
-                      </header>
-                      <ul>
-                        {me.cart.map(cartItem => (
-                          <CartItem key={cartItem.id} cartItem={cartItem} />
-                        ))}
-                      </ul>
-                      <footer>
-                        <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-                        <SickButton>Checkout</SickButton>
-                      </footer>
-                    </CartStyles>
-                  )}
-                </>
-              )}
-            </Query>
+        <>
+          {loading ? (
+            <p>Loading data</p>
+          ) : (
+            <CartStyles open={localState.data.cartOpen}>
+              {/* we are able to toggle the cart on the apollo state from here */}
+              <header>
+                <CloseButton title="close" onClick={toggleCart}>
+                  &times;
+                </CloseButton>
+                <Supreme>{me.name}'s Cart</Supreme>
+                <p>You have {me.cart.length} Item(s) in your cart.</p>
+              </header>
+              <ul>
+                {me.cart.map(cartItem => (
+                  <CartItem key={cartItem.id} cartItem={cartItem} />
+                ))}
+              </ul>
+              <footer>
+                <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+                <SickButton>Checkout</SickButton>
+              </footer>
+            </CartStyles>
           )}
-        </Mutation>
+        </>
       );
     }}
-  </User>
+  </Composed>
 );
 
 export default Cart;
