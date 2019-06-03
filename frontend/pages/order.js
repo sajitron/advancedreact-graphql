0a80d0efac6ad6c9f7678fec9813411239caf8b9
sajicode <sajioloye@gmail.com>
2019-06-03 23:43:04 +0100

    setup boilerplateb to display order

---
+++ frontend/pages/order.js
@@ -0,0 +1,11 @@
+import PleaseSignIn from "../components/PleaseSignIn";
+
+const Orderwww = props => (
+  <div>
+    <PleaseSignIn>
+      <p>This is a single order</p>
+    </PleaseSignIn>
+  </div>
+);
+
+export default Sell;
